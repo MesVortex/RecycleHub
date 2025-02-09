8b82ee88588decbcf86453576126e4cb80248618
MesVortex <meskinemostafa4@gmail.com>
2025-02-09 19:12:54 +0100

    REC-26 Implement submitted requests display

--- src/app/collection/list/list.component.ts
+++ src/app/collection/list/list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {CollectionRequest, CollectionState} from '../../store/collection/collection.state';
+import {deleteCollectionRequest, loadCollectionRequests} from '../../store/collection/collection.actions';
 
 @Component({
-  selector: 'app-list',
-  imports: [],
-  templateUrl: './list.component.html',
-  standalone: true,
-  styleUrl: './list.component.scss'
+  selector: 'app-collection-list',
+  templateUrl: './collection-list.component.html',
+  styleUrls: ['./collection-list.component.scss'],
+  standalone: true
 })
 export class CollectionListComponent {
+  requests$: Observable<CollectionRequest[]>;
 
+  constructor(private store: Store<{ collection: CollectionState }>) {
+    this.requests$ = this.store.select((state) => state.collection.requests);
+    this.store.dispatch(loadCollectionRequests()); // Load requests on init
+  }
+
+  onDeleteRequest(id: string) {
+    this.store.dispatch(deleteCollectionRequest({ id }));
+  }
 }
